1864a2cdc8da6bea34e144aef7afecaf5ef759c5
AlsoDanPiston <djpiston@gmail.com>
2021-11-17 17:41:42 -0500

    fixed sql script

--- server/client/src/helpers/BuildSqlScript.js
+++ server/client/src/helpers/BuildSqlScript.js
@@ -3,13 +3,6 @@ import React from 'react';
 export default function BuildSqlScript(obj, names) {
   let sqlSelectStr = '';
 
-  console.log(obj);
-  console.log(names);
-
-  // inputCols: (3) ['A', 'C', 'B']
-  // newColNameList: (3) ['a', 'c', 'b']
-  // newPositionList: (4) ['0', '2', '1', 'drop']
-
   for (let i = 0; i < obj.newColNameList.length; i++) {
     if (i < obj.newColNameList.length - 1) {
       sqlSelectStr += '  ' + names[obj.newPositionList[i]] + ' AS ' + obj.newColNameList[i] + ',\n';
